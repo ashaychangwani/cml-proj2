fe0802b5926769deabd1326d1283fe7b35a34804
Ashay Changwani <ashaychangwani@gmail.com>
2023-05-02 18:31:44 -0400

    Improve implementation

--- pytorch.py
+++ pytorch.py
@@ -4,6 +4,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 from statistics import mean
 
 # Matrix processing
@@ -16,6 +17,18 @@ final_matrix = pd.DataFrame(matrix3)
 # Importing the dataset
 X = final_matrix.iloc[1:, 0:26323].values
 y = final_matrix.iloc[1:, 26323].values
+
+# Convert the dataset to float32 dtype
+X = X.astype(np.float32)
+y = y.astype(np.float32)
+
+# Convert to tensors
+X = torch.tensor(X, dtype=torch.float32)
+y = torch.tensor(y, dtype=torch.float32).view(-1, 1)
+
+# Create TensorDataset and DataLoader
+dataset = TensorDataset(X, y)
+data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 # Building classifier (Fill with hyperparameters of choice)
 class Classifier(nn.Module):
@@ -41,11 +54,6 @@ def build_classifier():
 
 classifier, criterion, optimizer = build_classifier()
 
-X = X.astype(np.float32)
-X = torch.tensor(X)
-y = y.astype(np.float32)
-y = torch.tensor(y).view(-1, 1)
-
 # Check if CUDA is available and set the device
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Running on device:', device)
@@ -54,17 +62,18 @@ print('Running on device:', device)
 if torch.cuda.is_available():
     print('GPU name:', torch.cuda.get_device_name(device))
 
-# Move the model and data to the specified device
+# Move the model to the specified device
 classifier = classifier.to(device)
-X = X.to(device)
-y = y.to(device)
 
+# Training
 for epoch in range(45):
-    optimizer.zero_grad()
-    outputs = classifier(X)
-    loss = criterion(outputs, y)
-    loss.backward()
-    optimizer.step()
+    for batch_X, batch_y in data_loader:
+        batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+        optimizer.zero_grad()
+        outputs = classifier(batch_X)
+        loss = criterion(outputs, batch_y)
+        loss.backward()
+        optimizer.step()
 
 # Cross entropy
 def cross_entropy(pred, target, epsilon=1e-12):
@@ -73,17 +82,18 @@ def cross_entropy(pred, target, epsilon=1e-12):
     ce = -np.sum(target*np.log(pred+1e-9))/N
     return ce
 
-#Gene Importance
+# Gene Importance
 results = []
 
-y_pred_base = classifier(X).data.cpu().numpy()
+y_pred_base = classifier(X.to(device)).data.cpu().numpy()
 y_true = y.data.cpu().numpy().tolist()
 base_score = cross_entropy(y_pred_base, y_true)
+
 for kinase_number in range(len(X[0])):
     error_increase_list = []
     X_copy = X.clone().detach().cpu().numpy()
-    for shuffle_number in range (100):
-        np.random.shuffle(X_copy[:,kinase_number])
+    for shuffle_number in range(100):
+        np.random.shuffle(X_copy[:, kinase_number])
         X_shuffle = torch.tensor(X_copy, dtype=torch.float32).to(device)
         y_pred_new = classifier(X_shuffle).data.cpu().numpy()
         new_score = cross_entropy(y_pred_new, y_true)
@@ -91,10 +101,10 @@ for kinase_number in range(len(X[0])):
         error_increase_list.append(error_increase)
     mean_error = mean(error_increase_list)
     results.append(mean_error)
-    print(kinase_number,'/',len(X[0]))
+    print(kinase_number, '/', len(X[0]))
 
 results_df = pd.DataFrame(results)
 results_df['genes'] = matrix['Gene']
 
-#Export to excel
+# Export to excel
 results_df.to_excel('gene_importance.xlsx', sheet_name='1')
