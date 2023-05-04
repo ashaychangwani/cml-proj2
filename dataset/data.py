dfee4193df70bb2dae09de4b9613434ed54893b3
Ashay Changwani <ashaychangwani@gmail.com>
2023-05-03 20:11:11 -0400

    Changed dataset

--- dataset/data.py
+++ dataset/data.py
@@ -13,39 +13,24 @@ def prepare_cifar100_train_dataset(data_dir, dataset='cifar100', batch_size=128,
                                     shuffle=True, num_workers=4, pin_memory=True):
 
     train_transform = transforms.Compose([
-        transforms.RandomCrop(crop_size, padding),
+        transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.5071, 0.4865, 0.4409],
-        #                      std=[0.2673, 0.2564, 0.2762]),
-        transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
-                             std=[0.2023, 0.1994, 0.2010]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]),
     ])
-    train_dataset = torchvision.datasets.CIFAR100(root=data_dir, train=True, 
-                                                 download=True, transform=train_transform)
-    train_loader = torch.utils.data.DataLoader(train_dataset, 
-                                                batch_size=batch_size, 
-                                                shuffle=shuffle, 
-                                                num_workers=num_workers, 
-                                                pin_memory=pin_memory)
-    return train_loader
+    train_dataset = torchvision.datasets.ImageNet(root=data_dir, split='train', transform=train_transform)
+    print(len(train_dataset.classes))
+    return train_dataset
 
 def prepare_cifar100_test_dataset(data_dir, dataset='cifar100', batch_size=128, 
                                     shuffle=False, num_workers=4, pin_memory=True):
-    transform_test = transforms.Compose([
+    test_transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
-            # transforms.Normalize(mean=[0.5071, 0.4865, 0.4409],
-            #                      std=[0.2673, 0.2564, 0.2762]),
-            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
-                                 std=[0.2023, 0.1994, 0.2010]),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225]),
         ])
-    testset = torchvision.datasets.CIFAR100(root=data_dir,
-                                               train=False,
-                                               download=True,
-                                               transform=transform_test) 
-    test_loader = torch.utils.data.DataLoader(testset,
-                                                batch_size=batch_size,
-                                                shuffle=shuffle,
-                                                num_workers=num_workers,
-                                                pin_memory=pin_memory)  
-    return test_loader
+    testset = torchvision.datasets.ImageNet(root=data_dir, split='val', transform=test_transform)
+    return testset
